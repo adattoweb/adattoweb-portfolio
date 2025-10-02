58c8c9b5bca8f0aab12d564044d4bde3baa9075f
adattoweb <adattostudio174@gmail.com>
2025-10-02 20:38:02 +0300

    fixed bugs

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,11 +1,9 @@
 import './Footer.css'
 import adattologo from '../assets/images/adattologo.png'
 import github from '../assets/images/github.png'
-import telegram from '../assets/images/telegram.png'
-import discord from '../assets/images/discord.png'
 
 export default function Footer(props){
-    const {isEn} = props;
+    const { isEn } = props;
     return (
         <footer className="footer">
             <div className="footer-one">
@@ -23,14 +21,6 @@ export default function Footer(props){
             <div className="footer-two">
                 <a href="https://github.com/adattoweb" target="_blank" className="footer-two-item github">
                     <img src={github} alt="" />
-                </a>
-                <a href="https://t.me/adattoweb" target="_blank" className="footer-two-item telegram">
-                    <img src={telegram} alt="" />
-                </a>
-                <a onClick={() => {
-                    navigator.clipboard.writeText('@adattoweb')
-                }} href="#" className="footer-two-item discord">
-                    <img src={discord} alt="" />
                 </a>
             </div>
         </footer>
